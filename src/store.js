597cfd9aea449440cf4af0ef4e89bcd3741e6fb8
SMITH PENG (ITA-ISDC-ISD-OOCLL/ZHA) <PENGSM@OOCL.COM>
2019-07-25 11:20:48 +0800

    add something base component

--- src/store.js
+++ src/store.js
@@ -4,13 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        orderList: [],
+        statue: "all"
+    },
+    mutations: {
+        addNewItem(state, item) {
+            state.orderList.push(item)
+        }
+    }
 })
